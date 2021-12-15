a8dcda5583853421714c70c46cc5c93108aa0272
Simone Finocchietti <65085506+simofin96@users.noreply.github.com>
2021-12-15 01:40:16 +0100

    Multiple video compression and robustness improvement.
    Now it is possible to compress several videos at the same time and you have the possibility to insert the link to the videos simply by dragging them on the terminal screen.

--- video_record_compression.py
+++ video_record_compression.py
@@ -1,7 +1,7 @@
 import os 
 import subprocess 
 import time 
-import glob
+import glob 
 
 
 def video_recording():
@@ -18,39 +18,55 @@ def video_recording():
     print("Recording stopped!\n\r")
     list_of_videos = glob.glob(os.getcwd()+'\*.mkv') # * means all, if need specific format then *.format  
     latest_video = max(list_of_videos, key=os.path.getctime)
-    video_name, video_extension = os.path.splitext(latest_video)
-    # Use line 1 instead of line 2 if you want to keep the compressed video in the same format as the original one 
-    # output_video = video_name + "_compressed" + video_extension # line 1
-    output_video = video_name + ".mp4" # line 2
-    print("Starting compression!\n\r")
-    # Use line 3 instead of line 4 if you want to reduce the video resolution to 720p (in the case of horizontally oriented videos)
-    # subprocess.run(["ffmpeg", "-i", latest_video, "-preset", "veryfast", "-vf", "scale=-2:720", "-vcodec", "h264", "-acodec", "aac", output_video]) # line 3
-    subprocess.run(["ffmpeg", "-i", latest_video, "-preset", "veryfast", "-vcodec", "h264", "-acodec", "aac", output_video]) # line 4
-    os.remove(latest_video)
-    print("Compression done!")
-    
+    video_compression(latest_video)
 
-def video_compression():
-    input_video = input("Enter the name of the video you want to compress (file extension included)!\n\r")
-    video_name, video_extension = os.path.splitext(input_video)
-    # Use line 1 instead of line 2 if you want to keep the compressed video in the same format as the original one 
-    # output_video = video_name + "_compressed" + video_extension # line 1
-    output_video = video_name + "_compressed.mp4" # line 2
-    print("Starting compression!\n\r")
-    # Use line 3 instead of line 4 if you want to reduce the video resolution to 720p (in the case of horizontally oriented videos)
-    # subprocess.run(["ffmpeg", "-i", input_video, "-preset", "veryfast", "-vf", "scale=-2:720", "-vcodec", "h264", "-acodec", "aac", output_video]) # line 3
-    subprocess.run(["ffmpeg", "-i", input_video, "-preset", "veryfast", "-vcodec", "h264", "-acodec", "aac", output_video]) # line 4
-    print("Compression done!")
+
+def video_compression(input_video):
+    start_time = time.time() 
+    input_video_array = input_video.split('"')
+    if len(input_video_array) == 1:
+        video_name, video_extension = os.path.splitext(input_video_array[0])
+        # Use line 2 instead of line 1 if you want the compressed video to be always in mp4 format 
+        output_video = video_name + "_compressed" + video_extension # line 1
+        # output_video = video_name + "_compressed.mp4" # line 2
+        print("Starting compression!\n\r")
+        # Use line 4 instead of line 3 if you want to reduce the video resolution to 720p (in the case of horizontally oriented videos)
+        subprocess.run(["ffmpeg", "-i", input_video_array[0], "-preset", "veryfast", "-vcodec", "h264", "-acodec", "aac", output_video]) # line 3
+        # subprocess.run(["ffmpeg", "-i", input_video_array[0], "-preset", "veryfast", "-vf", "scale=-2:720", "-vcodec", "h264", "-acodec", "aac", output_video]) # line 4
+    else:
+        counter = 0
+        while(counter < len(input_video_array)):
+            video_name, video_extension = os.path.splitext(input_video_array[counter])
+            if(len(video_extension) != 0):
+                # Use line 2 instead of line 1 if you want the compressed video to be always in mp4 format 
+                output_video = video_name + "_compressed" + video_extension # line 1
+                # output_video = video_name + "_compressed.mp4" # line 2
+                print("Starting compression!\n\r")
+                # Use line 4 instead of line 3 if you want to reduce the video resolution to 720p (in the case of horizontally oriented videos)
+                subprocess.run(["ffmpeg", "-i", input_video_array[counter], "-preset", "veryfast", "-vcodec", "h264", "-acodec", "aac", output_video]) # line 3
+                # subprocess.run(["ffmpeg", "-i", input_video_array[counter], "-preset", "veryfast", "-vf", "scale=-2:720", "-vcodec", "h264", "-acodec", "aac", output_video]) # line 4
+            counter += 1
+    time_required = round(time.time()) - round(start_time)
+    if time_required < 60:
+        print("Compression done!\n--- %s seconds required ---" %time_required)
+    else:
+        minutes = time_required // 60
+        seconds = time_required - (minutes * 60)
+        print("Compression done!\n--- " + str(minutes) + " minutes and " + str(seconds) + " seconds required ---")
 
 
 if __name__ == "__main__":
     while True:  
-        tempvar = input("Do you want to record or compress a video? Valid: r (= record) / c (= compress)\n\r")
+        tempvar = input("Do you want to record or compress a video? Valid: r (= record) / c (= compress)\n")
         if(tempvar == "r"):
             video_recording()
             break       
         elif(tempvar == "c"):
-            video_compression()
-            break
+            input_video = input("Drag and drop here the videos you want to compress, or just enter their paths (file extension included)!\n")
+            if len(input_video) != 0:
+                video_compression(input_video)
+                break
+            else:   
+                print("No valid path has been entered! Choose again.\n")
         else: 
-            print("\n\rNot a valid command, choose again!\n\r")
+            print("Not a valid command! Choose again.\n")
